fced6b208461f6396935660afe81c8746588f7d7
sasha-filitovich <sasha.filitovich@mail.ru>
2023-05-06 18:03:10 +0300

    fix: change solution to 5th task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,23 +1,18 @@
 const fs = require('fs');
 const path = require('path');
-let arr = [];
-fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
-  if (err) console.log(err);
-  else
-    files.forEach((file) => {
-      if (file.isFile() && path.extname(path.join(__dirname, 'styles', file.name)) === '.css') {
-        fs.readFile(path.join(__dirname, 'styles', file.name), 'utf-8', (err, data) => {
-          if (err) console.log(err);
-          else {
-            arr.push(data);
-          }
-        });
-      }
-      console.log(arr);
-    });
-});
-const str = arr.join('\n');
 
-fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), str, (err) => {
-  if (err) throw err;
-});
+fs.promises
+  .readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
+  .then((files) =>
+    Promise.all(
+      files
+        .filter(
+          (file) =>
+            file.isFile() && path.extname(path.join(__dirname, 'styles', file.name)) === '.css'
+        )
+        .map((file) => fs.promises.readFile(path.join(__dirname, 'styles', file.name), 'utf-8'))
+    )
+  )
+  .then((data) => {
+    fs.promises.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), data.join('\n'));
+  });
