4292192be1b51889106c86b4655c89ed111cfd60
sasha-filitovich <sasha.filitovich@mail.ru>
2023-05-08 23:45:51 +0300

    feat: add solution to 5 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,18 @@
 const fs = require('fs/promises');
 const path = require('path');
 
+// (async () => {
+//   await fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+//   const filesToRemove = await fs.readdir(path.join(__dirname, 'files-copy'));
+//   await Promise.all(filesToRemove.map((file) => fs.rm(path.join(__dirname, 'files-copy', file))));
+//   const filesToCopy = await fs.readdir(path.join(__dirname, 'files'));
+//   await Promise.all(
+//     filesToCopy.map((file) =>
+//       fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file))
+//     )
+//   );
+// })();
+
 fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true })
   .then(() => fs.readdir(path.join(__dirname, 'files-copy')))
   .then((files) =>
---
+++ 06-build-page/index.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+const path = require('path');
+let template;
+let fileNames = [];
+let componentsObj = {};
+fs.promises
+  .readFile(path.join(__dirname, 'template.html'), 'utf-8')
+  .then((data) => (template = data))
+  .then(() => fs.promises.readdir(path.join(__dirname, 'components'), { withFileTypes: true }))
+  .then((files) => {
+    fileNames = files
+      .filter(
+        (file) =>
+          file.isFile() && path.extname(path.join(__dirname, 'components', file.name)) === '.html'
+      )
+      .map((file) => file.name);
+    return Promise.all(
+      files
+        .filter(
+          (file) =>
+            file.isFile() && path.extname(path.join(__dirname, 'components', file.name)) === '.html'
+        )
+        .map((file) => fs.promises.readFile(path.join(__dirname, 'components', file.name), 'utf-8'))
+    );
+  })
+  .then((data) => {
+    fileNames.forEach((el, index) => {
+      const name = el.split('.')[0];
+      componentsObj[name] = data[index];
+    });
+  })
+  .then(() => {
+    for (const key in componentsObj) {
+      template = template.replace(`{{${key}}}`, componentsObj[key]);
+    }
+  })
+  .then(() => fs.promises.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }))
+  .then(() => fs.promises.writeFile(path.join(__dirname, 'project-dist', 'index.html'), template))
+
+  .then(() => fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }))
+  .then((files) =>
+    Promise.all(
+      files
+        .filter(
+          (file) =>
+            file.isFile() && path.extname(path.join(__dirname, 'styles', file.name)) === '.css'
+        )
+        .map((file) => fs.promises.readFile(path.join(__dirname, 'styles', file.name), 'utf-8'))
+    )
+  )
+  .then((data) => {
+    fs.promises.writeFile(path.join(__dirname, 'project-dist', 'style.css'), data.join('\n'));
+  });
+
+function deleteDir(dir) {
+  fs.promises.readdir(dir, { withFileTypes: true }).then((files) => {
+    if (!files.length) {
+      fs.promises.rmdir(dir);
+    }
+    for (let file of files) {
+      if (file.isFile()) {
+        fs.promises.rm(dir, file.name);
+      } else {
+        deleteDir(path.join(dir, file.name));
+      }
+    }
+  });
+}
+deleteDir(path.join(__dirname, 'project-dist', 'assets'));
+// fs.promises.mkdir(path.join(__dirname, 'project-dist', 'assets'), { recursive: true });
+
+// fs.promises
+//   .readdir(path.join(__dirname, 'project-dist', 'assets'))
+//   .then((files) =>
+//     Promise.all(
+//       files.map((file) => fs.promises.rm(path.join(__dirname, 'project-dist', 'assets', file)))
+//     )
+//   );
+// function copyFile(dir, destination) {
+//   fs.promises.readdir(/*path.join(__dirname, 'assets' )*/ dir, { withFileTypes: true }).then(
+//     (files) => {
+//       for (let file of files) {
+//         if (!file.isFile()) {
+//           console.log(file);
+//           fs.promises
+//             .mkdir(/*path.join(__dirname, 'project-dist', 'assets')*/ destination, file.name, {
+//               recursive: true,
+//             })
+//             .then(() => copyFile(path.join(dir, file.name)));
+//         } else {
+//           fs.promises.copyFile(path.join(dir, file.name), path.join(__dirname, 'files-copy', file));
+//         }
+//       }
+//     }
+// Promise.all(
+//   files.map((file) =>
+//     fs.promises.copyFile(
+//       path.join(__dirname, 'files', file),
+//       path.join(__dirname, 'files-copy', file)
+//     )
+//   )
+// )
+//   );
+// }
+//copyFile(path.join(__dirname, 'assets'));
